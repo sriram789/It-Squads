afa644299942ced985cede98020481b950e67030
Sriram Nithyanandam <sriramdhoni12345@gmail.com>
2021-05-30 00:23:30 +0530

    'adding input component'

--- src/components/FunActivity/StartActivity/index.js
+++ src/components/FunActivity/StartActivity/index.js
@@ -1,9 +1,47 @@
 import React from 'react';
-import { StartActivityWrapper } from './style';
+import { StartActivityWrapper, EnterViewWrapper, SubmitButton } from './style';
 
-const StartActivity = () => {
+const USER_LIST = ['Jey', 'Arunachalam', 'Padmashri', 'Abishek', 'Anil', 'Aarthi', 'Aishwarya', 'Sriram'];
+
+const EnterView = ({user}) => {
   return (
-    <StartActivityWrapper>Seeing Start Activity</StartActivityWrapper>
+    <EnterViewWrapper>
+      <label>{user}</label>
+      <textarea placeholder={`Tell us about ${user}`}/>
+    </EnterViewWrapper>
+  );
+};
+
+const StartActivity = (props) => {
+  const submitHandler = (event) => {
+    event.preventDefault();
+    let resultData = [];
+    for(let i = 0; i<8; i++) {
+      let name = event.currentTarget.children[i].children[0].innerText;
+      let data = event.currentTarget.children[i].children[1].value;
+      let result = {
+        name,
+        data
+      };
+
+      resultData.push(result);
+    }
+    console.log(resultData);
+    props.history.push('/fun');
+  };
+
+  return (
+    <StartActivityWrapper>
+      <form onSubmit={submitHandler}>
+        {
+          USER_LIST.map((user) => {
+            return <EnterView user={user} key={user}/>
+          })
+        }
+        <SubmitButton>SUBMIT</SubmitButton>
+      </form>
+      
+    </StartActivityWrapper>
   )
 };
 
--- src/components/FunActivity/StartActivity/style.js
+++ src/components/FunActivity/StartActivity/style.js
@@ -1,3 +1,31 @@
 import styled from 'styled-components';
 
-export const StartActivityWrapper = styled.div``;
+export const StartActivityWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+`;
+
+export const EnterViewWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  border: 1px solid pink;
+  border-radius: 16px;
+  background-color: snow;
+  margin-bottom: 10px;
+  padding: 16px;
+
+  & label {
+    margin-bottom: 8px;
+  }
+`;
+
+export const SubmitButton = styled.button`
+  padding: 8px;
+  color: white;
+  background: darkblue;
+  border-radius: 8px;
+  width: max-content;
+  cursor: pointer;
+`;
